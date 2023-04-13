bab2916f71c47a70681cc6f72e40e1835bdd4afb
Chaitanya Paranjpe <112272151+cparanjpe@users.noreply.github.com>
2023-04-13 14:15:52 +0530

    homepage files

---
+++ stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Stories(){
+    return(
+        <div className="story">
+            <img src="https://cdn-useast1.kapwing.com/collections/video_image-7c0ZSfOAe.jpeg?Expires=1681012806&GoogleAccessId=dev-sa-videoprocessing%40kapwing-dev.iam.gserviceaccount.com&Signature=W8XOoOgfu4WPObiVN85K3PiAmkAH7mrLrEqZxWw%2B0z54Gn5DsFMuhCSw3xkgWDZs1AnZqTeOI7U41wT7FF4Dj9f85yMIXD9vTr%2FXJj35iZ8ZI5WZEhfACvqSbsUVkm9PDUY01Uacw3o7ezKuI89biXBaY783oSaN9ELz3c%2FfLkIlkyfFH81v16uSVie1VE%2FcB%2F7%2FaahGyTk6Qwgf1R5jzvocgf%2FtkD6QagLh1BlvcUdwjUOlMbhy5P3%2FCmYbpJnbHA0gLhMjdjOEAsy%2BKjxPtGzzTsCVkHMGk26ZmmH6AIbPDNits7T2uJ04gaT%2B%2Fz0ORaBPeTuhIcekmkebtR0jrA%3D%3D" className="rounded-full w-32" alt="" srcset="" />
+        </div>
+    )
+}
